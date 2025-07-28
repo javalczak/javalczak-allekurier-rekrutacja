af7cfb0b014011bce2c6155eca8f173e0db7097a
Marcin Walczak <javalczak@gmail.com>
2025-07-28 18:27:09 +0200

    feat(User): add isActive field and activate/deactivate logic

--- src/Core/User/Domain/User.php
+++ src/Core/User/Domain/User.php
@@ -25,14 +25,35 @@ class User
      */
     private string $email;
 
+    /**
+     * @ORM\Column(type="boolean", options={"default": false})
+     */
+    private bool $isActive;
+
     public function __construct(string $email)
     {
         $this->id = null;
         $this->email = $email;
+        $this->isActive = false;
     }
 
     public function getEmail(): string
     {
         return $this->email;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function activate(): void
+    {
+        $this->isActive = true;
+    }
+
+    public function deactivate(): void
+    {
+        $this->isActive = false;
     }
 }
